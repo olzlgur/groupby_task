1938593310ab37263a136d503f9520b2be1d3489
이지혁 <olzlgur@naver.com>
2022-12-16 21:44:24 +0900

    feat: scheam 구현

---
+++ exchangerate/graphql/schema/index.js
@@ -0,0 +1,33 @@
+const { gql } = require('apollo-server');
+const typeDefs = gql`
+type Query {
+    getExchangeRate(src:String!, tgt:String!): ExchangeInfo
+}
+
+type Mutation {
+    postExchangeRate(info: InputUpdateExchangeInfo): ExchangeInfo
+    deleteExchangeRate(info: InputDeleteExchangeInfo): ExchangeInfo
+}
+
+input InputUpdateExchangeInfo {
+    src: String!,
+    tgt: String!,
+    rate: Float!,
+    date: String
+}
+input InputDeleteExchangeInfo {
+    src: String!,
+    tgt: String!,
+    date: String!
+}
+
+type ExchangeInfo {
+    src: String!,
+    tgt: String!,
+    rate: Float!,
+    date: String!
+
+}`;
+
+module.exports = typeDefs
+
