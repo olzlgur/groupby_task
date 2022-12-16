d9075fbac6d2627c002b18e402e524281483ab24
이지혁 <olzlgur@naver.com>
2022-12-16 22:36:37 +0900

    docs: 주석 일부 수정

--- exchangerate/models/index.js
+++ exchangerate/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const MONGO_URL = `mongodb://localhost:27017/test`;
 
-// 데이터베이스 연결 모듈
+// 데이터베이스 연결 모듈 export
 module.exports = () => {
     mongoose.connect(MONGO_URL, {
         useNewUrlParser: true,
